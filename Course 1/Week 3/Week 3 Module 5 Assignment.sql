5ad6bc903ff755ecaa36163922fda0b33a53c9e4
UncleMichaelXiao <37294196+UncleMichaelXiao@users.noreply.github.com>
2020-04-16 17:53:18 +1200

    Update Week 3 Module 5 Assignment.sql

--- Course 1/Week 3/Week 3 Module 5 Assignment.sql
+++ Course 1/Week 3/Week 3 Module 5 Assignment.sql
@@ -1,6 +1,7 @@
-
+Question 1
 For event requests, list the event number, event date (eventrequest.dateheld), and count of the event plans.  
-Only include event requests in the result if the event request has more than one related event plan with a work date in December 2018.
+Only include event requests in the result if the event request has more than one 
+related event plan with a work date in December 2018.
 
 ---Oracle SQL Script---
 
